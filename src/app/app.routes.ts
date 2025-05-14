f85f99ecfd5d404135155d753e72d1c9f612f5e8
EduardTrave <u20211a789@upc.edu.pe>
2025-05-14 01:11:58 -0500

    Migración del proyecto original a la rama tasks-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
+// import { Routes } from '@angular/router';
+
+// export const routes: Routes = [];
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+  path: 'login',
+  loadComponent: () =>
+    import('./shared/components/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('./shared/components/tasks/tasks.component').then(m => m.TasksComponent)
+  },
+  {
+    path: 'task-details/:id',
+    loadComponent: () =>
+      import('./shared/components/task-details/task-details.component').then(m => m.TaskDetailsComponent)
+  }
+];
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,9 +3,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatSlideToggleModule,MatIconModule],
+  imports: [MatButtonModule, MatSlideToggleModule,MatIconModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
